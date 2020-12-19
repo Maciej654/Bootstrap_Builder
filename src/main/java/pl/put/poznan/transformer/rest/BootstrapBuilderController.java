155726d3c49c5680ac643bf0c6e983489f94f3a3
marmiko <maria.mikolajczakk@gmail.com>
2020-12-19 09:41:29 +0100

    Zmodyfikowano klasę kontrolera

--- src/main/java/pl/put/poznan/transformer/rest/BootstrapBuilderController.java
+++ src/main/java/pl/put/poznan/transformer/rest/BootstrapBuilderController.java
@@ -1,44 +1,33 @@
 package pl.put.poznan.transformer.rest;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.*;
-import pl.put.poznan.transformer.logic.BootstrapBuilder;
-
-import java.util.Arrays;
-
+import pl.put.poznan.transformer.logic.JSON.JSONContainer;
+import pl.put.poznan.transformer.logic.JSON.JSONContainerBuilder;
 
 @RestController
-@RequestMapping("/{text}")
+@RequestMapping("/bootstrap_builder")
 public class BootstrapBuilderController {
 
     private static final Logger logger = LoggerFactory.getLogger(BootstrapBuilderController.class);
 
-    @RequestMapping(method = RequestMethod.GET, produces = "application/json")
-    public String get(@PathVariable String text,
-                              @RequestParam(value="transforms", defaultValue="upper,escape") String[] transforms) {
 
-        // log the parameters
-        logger.debug(text);
-        logger.debug(Arrays.toString(transforms));
+    @RequestMapping(method = RequestMethod.GET)
+    @ResponseBody
+    public String get(@RequestBody String input) {
 
-        // perform the transformation, you should run your logic here, below is just a silly example
-        BootstrapBuilder transformer = new BootstrapBuilder(transforms);
-        return transformer.transform(text);
+        logger.debug(input);
+        System.out.println(input);
+        JSONContainer jsonContainer = null;
+        try {
+            jsonContainer = JSONContainerBuilder.createFromJSON(input);
+        } catch (JsonProcessingException e) {
+            e.printStackTrace();
+        }
+        return "ok";
     }
-
-    @RequestMapping(method = RequestMethod.POST, produces = "application/json")
-    public String post(@PathVariable String text,
-                      @RequestBody String[] transforms) {
-
-        // log the parameters
-        logger.debug(text);
-        logger.debug(Arrays.toString(transforms));
-
-        // perform the transformation, you should run your logic here, below is just a silly example
-        BootstrapBuilder transformer = new BootstrapBuilder(transforms);
-        return transformer.transform(text);
-    }
-
 
 
 }

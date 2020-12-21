a3bb3c8c37ad48ca59c8fd6bc201586a91536d45
wojkos9 <wojkos9@gmail.com>
2020-12-21 13:00:14 +0100

    Dodano dokumentację

--- src/main/java/pl/put/poznan/transformer/logic/BootstrapBuilder.java
+++ src/main/java/pl/put/poznan/transformer/logic/BootstrapBuilder.java
@@ -5,22 +5,37 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import pl.put.poznan.transformer.logic.html.HTMLDirector;
 
+/**
+ * Handles the creation of a Bootstrap HTML template from user input
+ * @author Wojciech Koszela
+ */
 public class BootstrapBuilder {
-    private final static String INVALUD_INPUT_MSG = "Error: Invalid input";
+    private final static String INVALID_INPUT_MSG = "Error: Invalid input";
 
     private static final Logger logger = LoggerFactory.getLogger(BootstrapBuilder.class);
     private StringBuilder specBuilder = new StringBuilder();
 
     public BootstrapBuilder(){}
 
+    /***
+     * Discards specification
+     */
     public void dropBuffer() {
         specBuilder = new StringBuilder();
     }
 
+    /***
+     * Add structure specification
+     * @param input specification in JSON format
+     */
     public void feedInput(String input) {
         specBuilder.append(input);
     }
 
+    /***
+     * Attempts to build a template based on provided input
+     * @return HTML code or error message
+     */
     public String makeResponse() {
         String response;
 
@@ -28,7 +43,7 @@ public class BootstrapBuilder {
             response = new HTMLDirector(specBuilder.toString()).constructHTML().toString();
         } catch (JsonProcessingException e) {
             logger.error(e.getMessage());
-            response = INVALUD_INPUT_MSG;
+            response = INVALID_INPUT_MSG;
         } finally {
             dropBuffer();
         }

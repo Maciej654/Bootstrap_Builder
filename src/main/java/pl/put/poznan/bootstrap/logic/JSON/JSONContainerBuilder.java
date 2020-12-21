3077c6a6f5a5a8698b9ed70e0b7def5ea6d2434d
Maciej654 <62801237+Maciej654@users.noreply.github.com>
2020-12-22 00:38:24 +0100

    dodanie odkumenajci

--- src/main/java/pl/put/poznan/bootstrap/logic/JSON/JSONContainerBuilder.java
+++ src/main/java/pl/put/poznan/bootstrap/logic/JSON/JSONContainerBuilder.java
@@ -10,6 +10,7 @@ public class JSONContainerBuilder {
      * reads fields from json file
      * @param json json file
      * @return object corresponding to the json input file
+     * @throws JsonProcessingException when json input is invalid
      */
     public static JSONContainer createFromJSON(String json) throws JsonProcessingException {
         return new ObjectMapper().readValue(json, JSONContainer.class);

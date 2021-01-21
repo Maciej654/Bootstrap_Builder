8fc4415c93e2a396cb06ba37bb7a33428a02db60
Maciej654 <62801237+Maciej654@users.noreply.github.com>
2021-01-21 20:45:12 +0100

    Fix InvalidDefinitionException error

--- src/main/java/pl/put/poznan/bootstrap/logic/JSON/JSONContainer.java
+++ src/main/java/pl/put/poznan/bootstrap/logic/JSON/JSONContainer.java
@@ -1,5 +1,6 @@
 package pl.put.poznan.bootstrap.logic.JSON;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
@@ -13,6 +14,7 @@ import java.util.Map;
  */
 @ToString
 @SuperBuilder
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class JSONContainer {
     @Getter @Setter
     private Map<String, String> metaTags;
@@ -25,4 +27,6 @@ public class JSONContainer {
 
     @Getter @Setter
     private String seotype;
+
+    public JSONContainer() {}
 }

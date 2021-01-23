c2b78dfd08e93a1164fe65f84c32c0b526447ce2
MMedzin <michal.medzin@protonmail.com>
2021-01-23 12:12:34 +0100

    Dodanie klasy przechowującej dane tagu meta

---
+++ src/main/java/pl/put/poznan/bootstrap/logic/html/components/MetaTag.java
@@ -0,0 +1,24 @@
+package pl.put.poznan.bootstrap.logic.html.components;
+
+/**
+ * class storing information about site's HTML meta tag
+ * @author Michał Mędzin
+ */
+public class MetaTag {
+
+    private String name;
+    private String content;
+
+    public MetaTag(String name, String content){
+        this.name = name;
+        this.content = content;
+    }
+
+    /**
+     * build and returns meta tag's HTML cod
+     * @return meta tag code in HTML
+     */
+    public String toString(){
+        return String.format("<meta name=\"%s\" content=\"%s\">", this.name, this.content);
+    }
+}

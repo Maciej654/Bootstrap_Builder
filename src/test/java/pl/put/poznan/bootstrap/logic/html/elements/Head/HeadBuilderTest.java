2eb8f34c5111ace3f31fbaf0db5cef17aa318c66
wojkos9 <wojkos9@gmail.com>
2021-01-24 12:47:55 +0100

    Usunięto niepotrzebne importy

--- src/test/java/pl/put/poznan/bootstrap/logic/html/elements/Head/HeadBuilderTest.java
+++ src/test/java/pl/put/poznan/bootstrap/logic/html/elements/Head/HeadBuilderTest.java
@@ -4,22 +4,6 @@ import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.junit.jupiter.api.Test;
-import org.w3c.dom.NodeList;
-import org.xml.sax.SAXException;
-
-import javax.xml.parsers.DocumentBuilder;
-import javax.xml.parsers.DocumentBuilderFactory;
-import javax.xml.parsers.ParserConfigurationException;
-import javax.xml.xpath.XPath;
-import javax.xml.xpath.XPathConstants;
-import javax.xml.xpath.XPathExpressionException;
-import javax.xml.xpath.XPathFactory;
-
-import java.io.IOException;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
-
-import static org.junit.jupiter.api.Assertions.*;
 
 class HeadBuilderTest {
 
@@ -33,6 +17,8 @@ class HeadBuilderTest {
 
         Document doc = Jsoup.parse(headBuilder.getHead().toString());
         Element styleNode = doc.selectFirst("head > style");
+        assert(styleNode != null);
+
         String html = styleNode.html();
         for (String s : styles) {
             assert(html.contains(s));
